a4ae71dcd8d4cab79d59ae2205c7cbedd89859ff
Prantaneel Pegu <159791377+Prantaneel-Pegu@users.noreply.github.com>
2025-01-01 15:26:39 +0530

    personal img update

--- frontend/src/components/AboutPage.tsx
+++ frontend/src/components/AboutPage.tsx
@@ -1,78 +1,133 @@
-import './styles/AboutPage.css';
-import DeveloperAvatar from '../assets/pictures/kookie.jpg';
-import { GithubIcon, IndeedIcon, LinkedInIcon, TwitterIcon } from './SvgIcons';
+import "./styles/AboutPage.css";
+import DeveloperAvatar from "../assets/pictures/my-image.jpg";
+import { GithubIcon, IndeedIcon, LinkedInIcon, TwitterIcon } from "./SvgIcons";
 
 function AboutPage() {
-  return (
-    <div id="AboutPage">
-        <h1 id="about-page-head">Hi There👋!</h1>
-        <br />
-        <p id="welcome-text">Welcome to the NASA Space Gallery.</p>
+    return (
+        <div id="AboutPage">
+            <h1 id="about-page-head">Hi There👋!</h1>
+            <br />
+            <p id="welcome-text">Welcome to the NASA Space Gallery.</p>
 
-        <p className="project-desc">
-          This is a <span className="project-tech">React + Typescript + Express</span> project that utilises the various NASA and Google Earth Engine APIs to present geographical and astronomical data in a clean and organised manner. I have spent countless hours (250+) in this project to make sure that this Gallery provides a neat and beautiful interface for accessing the awesome imagery hosted by NASA and Google. This website is fully optimised for desktop, laptop and mobile users and has a very intuitive UI.
-        </p>
+            <p className="project-desc">
+                This is a{" "}
+                <span className="project-tech">
+                    React + Typescript + Express
+                </span>{" "}
+                project that utilises the various NASA and Google Earth Engine
+                APIs to present geographical and astronomical data in a clean
+                and organised manner. I have spent countless hours (250+) in
+                this project to make sure that this Gallery provides a neat and
+                beautiful interface for accessing the awesome imagery hosted by
+                NASA and Google. This website is fully optimised for desktop,
+                laptop and mobile users and has a very intuitive UI.
+            </p>
 
-        <p className="project-desc">
-          I created this project mainly to showcase my full-stack development skills and my experience in working and synchronizing with enterprise grade APIs. The front-end is composed of a barebones HTML file which is populated through React hydration. I have used react-router to manage the client-side navigation so as to provide a snappy and seamless UI experience. The backend is powered by an Express.js server running on Node.js. All API secrets are handled in the backend itself and no secrets data is ever leaked to the client. 
-        </p>
+            <p className="project-desc">
+                I created this project mainly to showcase my full-stack
+                development skills and my experience in working with enterprise
+                grade APIs. The front-end is composed of a barebones HTML file
+                which is populated through React hydration. I have used
+                react-router to manage the client-side navigation so as to
+                provide a snappy and seamless UI experience. The backend is
+                powered by an Express.js server running on Node.js. All API
+                secrets are handled in the backend itself and no secrets data is
+                ever leaked to the client.
+            </p>
 
-        <p className="project-desc">
-          This project uses namely the following APIs:
-        </p>
+            <p className="project-desc">
+                This project uses namely the following APIs:
+            </p>
 
-        <ul className="about-api-list">
-          <li className="about-api-item">NASA APOD (Astronomy Picture of the Day) API</li>
-          <li className="about-api-item">NASA Image and Video Library API</li>
-          <li className="about-api-item">NASA Mars Rover Photos API</li>
-          <li className="about-api-item">Google Maps Static API</li>
-        </ul>
+            <ul className="about-api-list">
+                <li className="about-api-item">
+                    NASA APOD (Astronomy Picture of the Day) API
+                </li>
+                <li className="about-api-item">
+                    NASA Image and Video Library API
+                </li>
+                <li className="about-api-item">NASA Mars Rover Photos API</li>
+                <li className="about-api-item">Google Maps Static API</li>
+            </ul>
 
-        <p className="project-desc">
-          Working in this project has been an eye-opening experience. I have come to realise that many of the things we take for granted on websites (dark mode, seamless UI etc.) are actually the fruits of many hours of hard work by multiple devs. I am truly grateful that I took the courage to do this project because I have learnt a lot of invaluable things on this journey. This was my first large scale full-stack project and I am happy to say that I'm proud of the work that I have done. I still remember the many hours that I toiled away on this project, refusing to budge from my chair, even ignoring when my pomodoro alarms rang. I became really invested in this project. It quickly went from being a simple portfolio piece to one of my most loved pieces of work. I truly feel that doing this project has made me love web development even more.
-        </p>
+            <p className="project-desc">
+                Working in this project has been an a great learning experience.
+                It has given me a deep appreciation for the effort that goes
+                into building features we often take for granted on websites,
+                such as dark mode and seamless user interfaces. These seemingly
+                simple elements are the result of countless hours of dedication
+                by multiple developers. I am truly grateful for taking the
+                initiative to embark on this journey, as it has taught me
+                invaluable lessons. This project marks my first large-scale
+                full-stack endeavor, and I am proud of the work I’ve
+                accomplished. This experience has not only refined my skills but
+                has also deepened my passion for web development.
+            </p>
 
-        <p className="project-desc">
-          And back to you, my anonymous visitor. Thank you for taking the time to view my project and to read through this short anecdote of mine. I truly wish that you have had a satisfactory experience using this website. Feel free to let me know if there are any shortcomings you come to find. I would really love to hear about your experience with my website. As always, I'll be available at my Twitter (X) handle. Bye bye!
-        </p>
+            <p className="project-desc">
+                Thank you, my anonymous visitor, for taking the time to view my
+                project and to read through this short anecdote of mine. I truly
+                wish that you have had a satisfactory experience using this
+                website. Feel free to let me know if there are any shortcomings
+                you come to find. I would really love to hear about your
+                experience with my website. As always, I'll be available at my
+                Twitter (X) handle. Bye!
+            </p>
 
+            <div id="devcard-and-socials">
+                <div id="about-developer-card">
+                    <div id="adc-avatar-container">
+                        <img
+                            src={DeveloperAvatar}
+                            alt="A picture of me."
+                            id="adc-avatar"
+                        />
+                    </div>
 
-      <div id="devcard-and-socials">
-        <div id="about-developer-card">
-          <div id="adc-avatar-container">
-            <img src={DeveloperAvatar} alt="A picture of me." id="adc-avatar" />
-          </div>
+                    <p id="adc-handle">
+                        <a
+                            href="https://x.com/PrantaneelPegu"
+                            className="adc-handle-link"
+                        >
+                            @Prantaneel Pegu
+                        </a>
+                    </p>
+                </div>
 
-          <p id="adc-handle">
-            <a href="https://x.com/PrantaneelPegu" className="adc-handle-link">
-              @Prantaneel Pegu
-            </a>
-          </p>
+                <div id="about-socials">
+                    <div id="about-socials-container">
+                        <a
+                            href="https://github.com/Prantaneel-Pegu"
+                            className="about-social-link"
+                        >
+                            <GithubIcon classNames="about-social-icon" />
+                        </a>
+
+                        <a
+                            href="https://x.com/PrantaneelPegu"
+                            className="about-social-link"
+                        >
+                            <TwitterIcon classNames="about-social-icon" />
+                        </a>
+
+                        <a
+                            href="https://www.linkedin.com/in/prantaneel-pegu/"
+                            className="about-social-link"
+                        >
+                            <LinkedInIcon classNames="about-social-icon" />
+                        </a>
+
+                        <a
+                            href="https://profile.indeed.com/p/prantaneelp-jv4nj7f"
+                            className="about-social-link"
+                        >
+                            <IndeedIcon classNames="about-social-icon" />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div id="about-socials">
-          <div id="about-socials-container">
-            <a href="https://github.com/Prantaneel-Pegu" className="about-social-link">
-              <GithubIcon classNames="about-social-icon" />
-            </a>
-
-            <a href="https://x.com/PrantaneelPegu" className="about-social-link">
-              <TwitterIcon classNames="about-social-icon" />
-            </a>
-
-            <a href="https://www.linkedin.com/in/prantaneel-pegu/" className="about-social-link">
-              <LinkedInIcon classNames="about-social-icon" />
-            </a>
-
-            <a href="https://profile.indeed.com/p/prantaneelp-jv4nj7f" className="about-social-link">
-              <IndeedIcon classNames="about-social-icon" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
-
 
 export default AboutPage;
--- frontend/src/components/Copyright.tsx
+++ frontend/src/components/Copyright.tsx
@@ -1,47 +1,61 @@
-import './styles/Copyright.css';
-import { GithubIcon, IndeedIcon, LinkedInIcon, TwitterIcon } from './SvgIcons';
-import FooterAvatarImg from '../assets/pictures/kookie.jpg';
-import FooterRocketImg from '../assets/pictures/rocket.png';
+import "./styles/Copyright.css";
+import { GithubIcon, IndeedIcon, LinkedInIcon, TwitterIcon } from "./SvgIcons";
+import FooterAvatarImg from "../assets/pictures/my-image.jpg";
+import FooterRocketImg from "../assets/pictures/rocket.png";
 
-function Copyright () {
+function Copyright() {
     return (
-        <footer id="Copyright">     
-            <p id="footer-project-name">
-                NASA SPACE GALLERY
-            </p>
+        <footer id="Copyright">
+            <p id="footer-project-name">NASA SPACE GALLERY</p>
 
             <div id="footer-desc">
                 <p id="footer-desc-p">
-                    Hi there👋! This is a project I created for my portfolio. Take a look at my other projects <a href="https://github.com/Prantaneel-Pegu/Portfolio" id="footer-portfolio-link">here</a>.
+                    Hi there👋! This is a project I created for my portfolio.
+                    You can check out my other projects on{" "}
+                    <a
+                        href="https://www.linkedin.com/in/prantaneel-pegu/"
+                        id="footer-portfolio-link"
+                    >
+                        LinkedIn
+                    </a>
+                    .
                 </p>
             </div>
 
             <div id="footer-developer-card">
                 <div id="fdc-avatar-container">
-                    <img src={FooterAvatarImg} alt="A picture of me" id="fdc-avatar" />
+                    <img
+                        src={FooterAvatarImg}
+                        alt="A picture of me"
+                        id="fdc-avatar"
+                    />
                 </div>
                 <p id="footer-developer-handle">
-                    <a href="https://x.com/PrantaneelPegu" className="footer-dev-handle-link">
+                    <a
+                        href="https://x.com/PrantaneelPegu"
+                        className="footer-dev-handle-link"
+                    >
                         @Prantaneel Pegu
                     </a>
                 </p>
             </div>
 
             <div id="footer-useful-links">
-                <p id="ful-head">
-                    Useful Links:
-                </p>
+                <p id="ful-head">Useful Links:</p>
 
                 <div id="ful-container">
                     <a href="https://api.nasa.gov/" className="ful-link">
                         <p className="ful-link-text">NASA APIs Page</p>
                     </a>
-                    
+
                     <a href="https://images.nasa.gov/" className="ful-link">
                         <p className="ful-link-text">NASA Multimedia Library</p>
                     </a>
 
-                    <a href="https://developers.google.com/earth-engine" className="ful-link">
+                    <a
+                        href="https://developers.google.com/earth-engine"
+                        className="ful-link"
+                    >
                         <p className="ful-link-text">Google Earth Engine</p>
                     </a>
                 </div>
@@ -50,33 +64,49 @@ function Copyright () {
             <div id="footer-socials">
                 <p id="footer-socials-head">Follow me:</p>
                 <div id="footer-socials-container">
-                    <a href="https://github.com/Prantaneel-Pegu" className="footer-social-link">
+                    <a
+                        href="https://github.com/Prantaneel-Pegu"
+                        className="footer-social-link"
+                    >
                         <GithubIcon classNames="fsl-icon" />
                     </a>
 
-                    <a href="https://x.com/PrantaneelPegu" className="footer-social-link">
+                    <a
+                        href="https://x.com/PrantaneelPegu"
+                        className="footer-social-link"
+                    >
                         <TwitterIcon classNames="fsl-icon" />
                     </a>
 
-                    <a href="https://www.linkedin.com/in/prantaneel-pegu/" className="footer-social-link">
+                    <a
+                        href="https://www.linkedin.com/in/prantaneel-pegu/"
+                        className="footer-social-link"
+                    >
                         <LinkedInIcon classNames="fsl-icon" />
                     </a>
 
-                    <a href="https://profile.indeed.com/p/prantaneelp-jv4nj7f" className="footer-social-link">
+                    <a
+                        href="https://profile.indeed.com/p/prantaneelp-jv4nj7f"
+                        className="footer-social-link"
+                    >
                         <IndeedIcon classNames="fsl-icon" />
                     </a>
                 </div>
             </div>
 
             <div id="footer-container-img1">
-                <img src={FooterRocketImg} alt="A picture of a rocket" id="footer-img-1" />
+                <img
+                    src={FooterRocketImg}
+                    alt="A picture of a rocket"
+                    id="footer-img-1"
+                />
             </div>
 
             <p id="footer-copyright">
                 © Prantaneel Pegu, 2024. All rights reserved.
             </p>
         </footer>
-    )
+    );
 }
 
 export default Copyright;
