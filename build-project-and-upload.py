6bff642fdbdc57dbd81e863ac576fc7534cfa222
Prantaneel Pegu <159791377+Prantaneel-Pegu@users.noreply.github.com>
2025-08-19 00:28:15 +0530

    link update

--- build-project-and-upload.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-if (len(sys.argv) < 2):
-    print("ERR: No commit message supplied.")
-    exit(1)
-
-os.system("python ./build-project.py && python ./git-upload.py \"" + sys.argv[1] + "\"")
--- frontend/src/components/ColorSchemeToggle.tsx
+++ frontend/src/components/ColorSchemeToggle.tsx
@@ -1,42 +1,52 @@
-import './styles/ColorSchemeToggle.css';
-import { GithubIcon, MoonIcon, SunIcon } from './SvgIcons';
+import "./styles/ColorSchemeToggle.css";
+import { GithubIcon, MoonIcon, SunIcon } from "./SvgIcons";
 
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
-function enableDarkMode () {
-    const toggleDark = document.getElementsByClassName('toggle-dark');
-    const toggleLight = document.getElementsByClassName('toggle-light');
+function enableDarkMode() {
+    const toggleDark = document.getElementsByClassName("toggle-dark");
+    const toggleLight = document.getElementsByClassName("toggle-light");
 
-    body?.classList.add('dark-mode');
+    body?.classList.add("dark-mode");
 
     for (let i = 0; i < toggleDark.length; i++) {
-        toggleDark[i].classList.add('toggle-hidden');
-        toggleLight[i].classList.remove('toggle-hidden');
+        toggleDark[i].classList.add("toggle-hidden");
+        toggleLight[i].classList.remove("toggle-hidden");
     }
 }
 
-function disableDarkMode () {
-    const toggleDark = document.getElementsByClassName('toggle-dark');
-    const toggleLight = document.getElementsByClassName('toggle-light');
+function disableDarkMode() {
+    const toggleDark = document.getElementsByClassName("toggle-dark");
+    const toggleLight = document.getElementsByClassName("toggle-light");
 
-    body?.classList.remove('dark-mode');
+    body?.classList.remove("dark-mode");
 
     for (let i = 0; i < toggleDark.length; i++) {
-        toggleDark[i].classList.remove('toggle-hidden');
-        toggleLight[i].classList.add('toggle-hidden');
+        toggleDark[i].classList.remove("toggle-hidden");
+        toggleLight[i].classList.add("toggle-hidden");
     }
 }
 
-function ColorSchemeToggle () {
+function ColorSchemeToggle() {
     return (
         <div className="ColorSchemeToggle">
-            <SunIcon classNames='cst-sun-icon icon toggle-light' onClick={disableDarkMode} />
-            <MoonIcon classNames='cst-moon-icon icon toggle-dark toggle-hidden' onClick={enableDarkMode} />
-            <a href="https://github.com/Prantaneel-Pegu" className="cst-github-link">
-                <GithubIcon classNames='cst-github-icon icon'/>
+            <SunIcon
+                classNames="cst-sun-icon icon toggle-light"
+                onClick={disableDarkMode}
+            />
+            <MoonIcon
+                classNames="cst-moon-icon icon toggle-dark toggle-hidden"
+                onClick={enableDarkMode}
+            />
+            <a
+                href="https://github.com/Prantaneel-Pegu"
+                target="_blank"
+                className="cst-github-link"
+            >
+                <GithubIcon classNames="cst-github-icon icon" />
             </a>
         </div>
-    )
+    );
 }
 
 export default ColorSchemeToggle;
